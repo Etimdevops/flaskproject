8d36aaef55fcc0faa90072d5c5d52f9d74c5acbf
Etimdevops <142466894+Etimdevops@users.noreply.github.com>
2024-09-02 17:04:20 -0500

    Update flask_project.py

--- flask_project.py
+++ flask_project.py
@@ -7,4 +7,4 @@ def home():
     return "I am almost a DevOps Engineer!"
 
 if __name__ == '__main__':
-    app.run(host='172.31.95.41', port=5000)  # Bind to all interfaces and specify port
+    app.run(host='0.0.0.0', port=5000)  # Bind to all interfaces and specify port
